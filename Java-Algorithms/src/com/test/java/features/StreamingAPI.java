6323c66d6e8fe7810d43c597c072f121b3ba8b33
rbansode05 <rbansode05@yahoo.com>
2018-07-28 21:52:47 +0530

    First commit

---
+++ Java-Algorithms/src/com/test/java/features/StreamingAPI.java
@@ -0,0 +1,24 @@
+package com.test.java.features;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamingAPI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("StreamingAPI.main()");
+		List<String> stringList = new ArrayList<>();
+		stringList.add("ABC");
+		stringList.add("abc");
+		stringList.add("ABC");
+		stringList.add("DEF");
+		stringList.add("ABCD");
+		
+		
+		System.out.println("StreamingAPI.main() "+stringList);
+									
+		
+	}
+
+}
