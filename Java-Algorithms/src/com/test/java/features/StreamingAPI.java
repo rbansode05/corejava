65c3d4f8f80e20f101b1c3a4b6d9014d4ff1e56e
rahulb <rahulb@rahulb-A780LB>
2018-08-05 10:18:45 +0530

    Strem : Distinct Feature

--- Java-Algorithms/src/com/test/java/features/StreamingAPI.java
+++ Java-Algorithms/src/com/test/java/features/StreamingAPI.java
@@ -1,22 +1,24 @@
 package com.test.java.features;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamingAPI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("StreamingAPI.main()");
-		List<String> stringList = new ArrayList<>();
-		stringList.add("ABC");
-		stringList.add("abc");
-		stringList.add("ABC");
-		stringList.add("DEF");
-		stringList.add("ABCD");
+		List<String> stringList = Arrays.asList("Rahul", "Vishal", "Pravin", "Rahul");
+		System.out.println("Original List "+stringList);
+		
+		List<String> distinctList = stringList.stream()
+											 .distinct()
+											 .collect(Collectors.toList());
 		
 		
-		System.out.println("StreamingAPI.main() "+stringList);
+		System.out.println("distinctList "+distinctList);
+		
 									
 		
 	}
